76ab7587cbd2c841da7d35d46d7ce9a8778ed9b5
Jignesh Bhimani <jignesh.bhimani@trootech.com>
2025-03-19 18:42:59 +0530

    Initial setup for redux-store

---
+++ app/redux/store.ts
@@ -0,0 +1,57 @@
+import { configureStore } from '@reduxjs/toolkit';
+import { persistStore, persistReducer } from 'redux-persist';
+import createWebStorage from 'redux-persist/es/storage/createWebStorage';
+import { combineReducers } from 'redux';
+
+const createNoopStorage = () => {
+  return {
+    getItem(): Promise<null> {
+      return Promise.resolve(null)
+    },
+    setItem(_key: string, value: any): Promise<any> {
+      return Promise.resolve(value)
+    },
+    removeItem(_key: string): Promise<void> {
+      return Promise.resolve()
+    },
+  }
+}
+
+const storage =
+  typeof window !== 'undefined'
+    ? createWebStorage('local')
+    : createNoopStorage()
+
+const persistConfig = {
+  key: 'root',
+  storage,
+  // whitelist: ["auth"], // specify reducers to persist
+}
+
+const rootReducer = combineReducers({
+  // auth: authReducer,
+})
+
+const persistedReducer = persistReducer(persistConfig, rootReducer)
+
+export const store = configureStore({
+  reducer: persistedReducer,
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware({
+      serializableCheck: {
+        ignoredActions: [
+          'persist/PERSIST',
+          'persist/REHYDRATE',
+          'persist/PAUSE',
+          'persist/PURGE',
+          'persist/FLUSH',
+          'persist/REGISTER',
+        ],
+      },
+    }),
+})
+
+export const persistor = persistStore(store)
+
+export type RootState = ReturnType<typeof rootReducer>
+export type AppDispatch = typeof store.dispatch
