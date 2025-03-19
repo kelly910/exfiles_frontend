3faea4df1959bf2d9db082c07b040df65d11b6f6
Rahul Solanki <rahul.solanki@trootech.com>
2025-03-19 10:41:49 +0530

    Sample Pages setup, Import alias

---
+++ app/components/Signup/Signup.tsx
@@ -0,0 +1,7 @@
+'use client'
+
+import { Typography } from '@mui/material'
+
+export default function Login() {
+  return <Typography>This is the sample Signup Component</Typography>
+}
---
+++ app/signup/page.tsx
@@ -0,0 +1,14 @@
+import dynamic from 'next/dynamic'
+
+const DynamicSignupComponent = dynamic(
+  () => import('@components/Signup/Signup')
+)
+
+export default function Page() {
+  return (
+    <main>
+      <h1>SignUp page</h1>
+      <DynamicSignupComponent />
+    </main>
+  )
+}
