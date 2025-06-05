b07828d6be248bda05a615bc104dbe8fe830f7f0
pravintrootech <pravin.lagariya@trootech.com>
2025-06-05 15:48:32 +0530

    DocumentSummaryPopup layout and styles for improved readability and responsiveness

--- app/components/Download-Doc-Report/DocumentSummaryPopup.tsx
+++ app/components/Download-Doc-Report/DocumentSummaryPopup.tsx
@@ -78,11 +78,29 @@ export default function DocumentSummaryPopup({
                 height={28}
               />
             </Box>
-            <Typography variant="h6" className={LogStyle.dialogTitle}>
-              {passDocumentSummary?.file_name}
-            </Typography>
+            <Box>
+              <Typography variant="h6" className={LogStyle.dialogTitle}>
+                {passDocumentSummary?.file_name}
+              </Typography>
+              {/* <Typography variant="body1" className={LogStyle.dialogSemiTitle}>
+                {passDocumentSummary?.file_name}
+              </Typography> */}
+            </Box>
           </DialogTitle>
-          <IconButton aria-label="close" onClick={onClose}>
+          <IconButton
+            aria-label="close"
+            onClick={onClose}
+            sx={(theme) => ({
+              position: 'absolute',
+              right: '24px',
+              top: '16px',
+              padding: '0',
+              color: theme.palette.grey[500],
+              [theme.breakpoints.down('sm')]: {
+                right: '16px',
+              },
+            })}
+          >
             <Image
               src="/images/close.svg"
               alt="close-icon"
@@ -93,6 +111,9 @@ export default function DocumentSummaryPopup({
         </Box>
 
         <DialogContent className={LogStyle.dialogFormContentDetailsBox}>
+          {/* <Typography variant="body1" className={LogStyle.logTitle}>
+            Test
+          </Typography> */}
           <Box component="div" className={LogStyle.logListBody}>
             {passDocumentSummary?.tags?.map((tag, index) => (
               <Box className={LogStyle.logListBodyTag} key={index}>
@@ -116,39 +137,46 @@ export default function DocumentSummaryPopup({
               {convertDateFormat(passDocumentSummary?.upload_on || '') || '-'}
             </Typography>
           </Box>
-          {passDocumentSummary?.ai_description && (
-            <Box component="div" className={LogStyle.logListFooter}>
+          <Box component="div" className={LogStyle.logDetailsList}>
+            {passDocumentSummary?.ai_description && (
+              <Box component="div" className={LogStyle.logDetailsListInner}>
+                <Typography
+                  variant="body1"
+                  className={LogStyle.logDetailsListTitle}
+                >
+                  Description:
+                </Typography>
+                <Box component="div" className={LogStyle.logDetailsListDetails}>
+                  <Typography
+                    variant="body1"
+                    className={LogStyle.logDetailsListDetailsInner}
+                    dangerouslySetInnerHTML={{
+                      __html: processTextSummary(
+                        passDocumentSummary?.ai_description || '-'
+                      ),
+                    }}
+                  />
+                </Box>
+              </Box>
+            )}
+            <Box component="div" className={LogStyle.logDetailsListInner}>
               <Typography
                 variant="body1"
-                className={LogStyle.logListFooterTitle}
+                className={LogStyle.logDetailsListTitle}
               >
-                Description:
+                Summary Generated:
               </Typography>
-              <Typography
-                variant="body1"
-                className={LogStyle.logListFooterDetails}
-                dangerouslySetInnerHTML={{
-                  __html: processTextSummary(
-                    passDocumentSummary?.ai_description || '-'
-                  ),
-                }}
-              />
-            </Box>
-          )}
-          <Box component="div" className={LogStyle.logListFooter}>
-            <Typography variant="body1" className={LogStyle.logListFooterTitle}>
-              Summary Generated:
-            </Typography>
-            <Box component="div" className={LogStyle.logListFooter}>
-              <Typography
-                variant="body1"
-                className={LogStyle.logListFooterDetails}
-                dangerouslySetInnerHTML={{
-                  __html: processTextSummary(
-                    passDocumentSummary?.summary || '-'
-                  ),
-                }}
-              />
+              <Box component="div" className={LogStyle.logDetailsListDetails}>
+                <Typography
+                  variant="body1"
+                  className={LogStyle.logDetailsListDetailsInner}
+                  dangerouslySetInnerHTML={{
+                    __html: processTextSummary(
+                      passDocumentSummary?.summary || '-'
+                    ),
+                  }}
+                />
+              </Box>
             </Box>
           </Box>
         </DialogContent>
