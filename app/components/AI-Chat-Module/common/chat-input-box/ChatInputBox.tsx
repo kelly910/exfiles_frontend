c52961b2c5394245749e3476bbd9819e213bb524
Shubhangee <shubhangee.patil@trootech.com>
2025-07-09 10:41:40 +0530

    Expired dialog open while add incident and sending msg

--- app/components/AI-Chat-Module/common/chat-input-box/ChatInputBox.tsx
+++ app/components/AI-Chat-Module/common/chat-input-box/ChatInputBox.tsx
@@ -37,6 +37,7 @@ import { ErrorResponse, handleError } from '@/app/utils/handleError';
 import { useSelector } from 'react-redux';
 import { selectFetchedUser } from '@/app/redux/slices/login';
 import LimitOver from '@/app/components/Limit-Over/LimitOver';
+import PlanExpired from '@/app/components/Plan-Expired/PlanExpired';
 
 const DynamicDocUploadModal = dynamic(
   () => import('@components/AI-Chat-Module/modals/FileUploadDialog')
@@ -66,7 +67,7 @@ export default function ChatInputBox({
 
   const [limitDialog, setLimitDialog] = useState(false);
   const [limitType, setLimitType] = useState('');
-
+  const [expiredDialog, setExpireDialog] = useState(false);
   const { handleFiles } = useChunkedFileUpload((limitExceededType: string) => {
     setLimitType(limitExceededType);
     setLimitDialog(true);
@@ -184,48 +185,52 @@ export default function ChatInputBox({
   };
 
   const handleMessageSending = async () => {
-    if (chatUsedCheck) {
-      setLimitDialog(true);
+    if (expiredStatus === 0) {
+      setExpireDialog(true);
     } else {
-    if (uploadedFiles && uploadedFiles?.length > 0) {
-      const payloadDocs = uploadedFiles
-        .filter(({ uploadedFileId }) => typeof uploadedFileId === 'number')
-        .map(({ uploadedFileId, docDesc }) => ({
-          temp_doc: uploadedFileId as number,
-          description: docDesc,
-        }));
-
-      if (payloadDocs.length === 0) return false; // Exit early if no valid files
-
-      let createdThreadID;
-
-      if (threadId) {
-        createdThreadID = threadId;
+      if (chatUsedCheck) {
+        setLimitDialog(true);
       } else {
-        // Create New Thread
-        const resultData = await dispatch(createNewThread({}));
+        if (uploadedFiles && uploadedFiles?.length > 0) {
+          const payloadDocs = uploadedFiles
+            .filter(({ uploadedFileId }) => typeof uploadedFileId === 'number')
+            .map(({ uploadedFileId, docDesc }) => ({
+              temp_doc: uploadedFileId as number,
+              description: docDesc,
+            }));
 
-        if (createNewThread.rejected.match(resultData)) {
-          showToast('error', 'Something went wrong. Please try again!');
-          console.error('createNewThread failed:', resultData.payload);
-          return;
+          if (payloadDocs.length === 0) return false; // Exit early if no valid files
+
+          let createdThreadID;
+
+          if (threadId) {
+            createdThreadID = threadId;
+          } else {
+            // Create New Thread
+            const resultData = await dispatch(createNewThread({}));
+
+            if (createNewThread.rejected.match(resultData)) {
+              showToast('error', 'Something went wrong. Please try again!');
+              console.error('createNewThread failed:', resultData.payload);
+              return;
+            }
+
+            createdThreadID = resultData.payload?.uuid;
+          }
+
+          if (!createdThreadID) return;
+
+          // Upload documents
+          uploadActualDocuments(createdThreadID, payloadDocs);
+        } else {
+          const message = {
+            message_type: QUESTION_TYPES.QUESTION,
+            message: text,
+          };
+          sendMessage(message);
+          setText('');
         }
-
-        createdThreadID = resultData.payload?.uuid;
       }
-
-      if (!createdThreadID) return;
-
-      // Upload documents
-      uploadActualDocuments(createdThreadID, payloadDocs);
-    } else {
-      const message = {
-        message_type: QUESTION_TYPES.QUESTION,
-        message: text,
-      };
-      sendMessage(message);
-      setText('');
-    }
     }
   };
 
@@ -264,155 +269,155 @@ export default function ChatInputBox({
 
   return (
     <>
-    <Box component="div" className={AIChatStyles.chatBoard}>
-      <Box className={AIChatStyles.fileInput}>
-        {uploadedFiles && uploadedFiles?.length > 0 && (
-          <Box component="div" className={AIChatStyles.fileBoxDrag}>
-            {uploadedFiles.map((fileData) => (
-              <UploadFileItem
-                key={fileData.fileId}
-                fileName={fileData.file.name}
-                fileSize={fileData.file.size}
-                progress={fileData.progress}
-                hasUploaded={fileData.hasUploaded}
-                fileErrorMsg={fileData.fileErrorMsg}
-                hasError={fileData.hasError}
-                fileId={fileData.fileId}
-                fileDesc={fileData.docDesc}
-                onRemove={() => removeFile(fileData.fileId)}
-                handleFileDesc={handleFileDesc}
-              />
-            ))}
-          </Box>
-        )}
-        <Input
-          id="input-with-icon-adornment"
-          className={AIChatStyles.fileInputInner}
-          placeholder={
-            expiredStatus === 0
-              ? 'Your Plan has Expired'
-              : chatUsedCheck
-                ? 'Your copilot Chats Limit is Over'
-                : 'Write your question here'
-          }
-          value={text}
-          onChange={handleText}
-          onKeyDown={handleKeyUp}
-          onPaste={handlePaste}
-          multiline
-          minRows={1}
-          maxRows={3}
-          disabled={!fetchedUser?.staff_user && expiredStatus === 0}
-          style={{ width: '100%' }}
-          sx={{
-            '.Mui-disabled': {
-              color: 'var(--Subtext-Color) !important',
-              WebkitTextFillColor: 'var(--Subtext-Color) !important',
-            },
-          }}
-          endAdornment={
-            <InputAdornment
-              position="end"
-              className={`${AIChatStyles.fileIcon} ${fetchedUser?.staff_user !== true && expiredStatus === 0 ? 'limitation-icon' : ''}`}
-              onClick={handleOpenDocUploadModal}
-              disablePointerEvents={
-                !fetchedUser?.staff_user && expiredStatus === 0
-              }
-            >
-              <span className={AIChatStyles.clip}></span>
-            </InputAdornment>
-          }
-          startAdornment={
-            !fetchedUser?.staff_user &&
-            expiredStatus === 0 && (
-              <InputAdornment position="start">
-                <span style={{ display: 'flex', alignItems: 'center' }}>
-                  <svg
-                    width="18"
-                    height="18"
-                    viewBox="0 0 24 24"
-                    fill="none"
-                    xmlns="http://www.w3.org/2000/svg"
-                  >
-                    <path
-                      d="M21.76 15.92L15.36 4.4C14.5 2.85 13.31 2 12 2C10.69 2 9.49998 2.85 8.63998 4.4L2.23998 15.92C1.42998 17.39 1.33998 18.8 1.98998 19.91C2.63998 21.02 3.91998 21.63 5.59998 21.63H18.4C20.08 21.63 21.36 21.02 22.01 19.91C22.66 18.8 22.57 17.38 21.76 15.92ZM11.25 9C11.25 8.59 11.59 8.25 12 8.25C12.41 8.25 12.75 8.59 12.75 9V14C12.75 14.41 12.41 14.75 12 14.75C11.59 14.75 11.25 14.41 11.25 14V9ZM12.71 17.71C12.66 17.75 12.61 17.79 12.56 17.83C12.5 17.87 12.44 17.9 12.38 17.92C12.32 17.95 12.26 17.97 12.19 17.98C12.13 17.99 12.06 18 12 18C11.94 18 11.87 17.99 11.8 17.98C11.74 17.97 11.68 17.95 11.62 17.92C11.56 17.9 11.5 17.87 11.44 17.83C11.39 17.79 11.34 17.75 11.29 17.71C11.11 17.52 11 17.26 11 17C11 16.74 11.11 16.48 11.29 16.29C11.34 16.25 11.39 16.21 11.44 16.17C11.5 16.13 11.56 16.1 11.62 16.08C11.68 16.05 11.74 16.03 11.8 16.02C11.93 15.99 12.07 15.99 12.19 16.02C12.26 16.03 12.32 16.05 12.38 16.08C12.44 16.1 12.5 16.13 12.56 16.17C12.61 16.21 12.66 16.25 12.71 16.29C12.89 16.48 13 16.74 13 17C13 17.26 12.89 17.52 12.71 17.71Z"
-                      fill="#E72240"
-                    />
-                  </svg>
-                </span>
+      <Box component="div" className={AIChatStyles.chatBoard}>
+        <Box className={AIChatStyles.fileInput}>
+          {uploadedFiles && uploadedFiles?.length > 0 && (
+            <Box component="div" className={AIChatStyles.fileBoxDrag}>
+              {uploadedFiles.map((fileData) => (
+                <UploadFileItem
+                  key={fileData.fileId}
+                  fileName={fileData.file.name}
+                  fileSize={fileData.file.size}
+                  progress={fileData.progress}
+                  hasUploaded={fileData.hasUploaded}
+                  fileErrorMsg={fileData.fileErrorMsg}
+                  hasError={fileData.hasError}
+                  fileId={fileData.fileId}
+                  fileDesc={fileData.docDesc}
+                  onRemove={() => removeFile(fileData.fileId)}
+                  handleFileDesc={handleFileDesc}
+                />
+              ))}
+            </Box>
+          )}
+          <Input
+            id="input-with-icon-adornment"
+            className={AIChatStyles.fileInputInner}
+            placeholder={
+              expiredStatus === 0
+                ? 'Your Plan has Expired'
+                : chatUsedCheck
+                  ? 'Your copilot Chats Limit is Over'
+                  : 'Write your question here'
+            }
+            value={text}
+            onChange={handleText}
+            onKeyDown={handleKeyUp}
+            onPaste={handlePaste}
+            multiline
+            minRows={1}
+            maxRows={3}
+            style={{ width: '100%' }}
+            sx={{
+              '.Mui-disabled': {
+                color: 'var(--Subtext-Color) !important',
+                WebkitTextFillColor: 'var(--Subtext-Color) !important',
+              },
+            }}
+            endAdornment={
+              <InputAdornment
+                position="end"
+                className={`${AIChatStyles.fileIcon} ${fetchedUser?.staff_user !== true && expiredStatus === 0 ? 'limitation-icon' : ''}`}
+                onClick={handleOpenDocUploadModal}
+                disablePointerEvents={
+                  !fetchedUser?.staff_user && expiredStatus === 0
+                }
+              >
+                <span className={AIChatStyles.clip}></span>
               </InputAdornment>
-            )
-          }
-        />
-      </Box>
-
-      <Button
-        type="button"
-        variant="contained"
-        className={`btn-arrow ${fetchedUser?.staff_user !== true && expiredStatus === 0 ? 'limitation' : ''}`}
-        color="primary"
-        fullWidth
-        onClick={handleMessageSending}
-        disabled={
-          isSendDisabled ||
-          (fetchedUser?.staff_user !== true && expiredStatus === 0)
-        }
-      >
-        {isLoadingProp ? (
-          <CircularProgress
-            size={18}
-            sx={{ color: 'var(--Txt-On-Gradient)' }}
+            }
+            startAdornment={
+              !fetchedUser?.staff_user &&
+              expiredStatus === 0 && (
+                <InputAdornment position="start">
+                  <span style={{ display: 'flex', alignItems: 'center' }}>
+                    <svg
+                      width="18"
+                      height="18"
+                      viewBox="0 0 24 24"
+                      fill="none"
+                      xmlns="http://www.w3.org/2000/svg"
+                    >
+                      <path
+                        d="M21.76 15.92L15.36 4.4C14.5 2.85 13.31 2 12 2C10.69 2 9.49998 2.85 8.63998 4.4L2.23998 15.92C1.42998 17.39 1.33998 18.8 1.98998 19.91C2.63998 21.02 3.91998 21.63 5.59998 21.63H18.4C20.08 21.63 21.36 21.02 22.01 19.91C22.66 18.8 22.57 17.38 21.76 15.92ZM11.25 9C11.25 8.59 11.59 8.25 12 8.25C12.41 8.25 12.75 8.59 12.75 9V14C12.75 14.41 12.41 14.75 12 14.75C11.59 14.75 11.25 14.41 11.25 14V9ZM12.71 17.71C12.66 17.75 12.61 17.79 12.56 17.83C12.5 17.87 12.44 17.9 12.38 17.92C12.32 17.95 12.26 17.97 12.19 17.98C12.13 17.99 12.06 18 12 18C11.94 18 11.87 17.99 11.8 17.98C11.74 17.97 11.68 17.95 11.62 17.92C11.56 17.9 11.5 17.87 11.44 17.83C11.39 17.79 11.34 17.75 11.29 17.71C11.11 17.52 11 17.26 11 17C11 16.74 11.11 16.48 11.29 16.29C11.34 16.25 11.39 16.21 11.44 16.17C11.5 16.13 11.56 16.1 11.62 16.08C11.68 16.05 11.74 16.03 11.8 16.02C11.93 15.99 12.07 15.99 12.19 16.02C12.26 16.03 12.32 16.05 12.38 16.08C12.44 16.1 12.5 16.13 12.56 16.17C12.61 16.21 12.66 16.25 12.71 16.29C12.89 16.48 13 16.74 13 17C13 17.26 12.89 17.52 12.71 17.71Z"
+                        fill="#E72240"
+                      />
+                    </svg>
+                  </span>
+                </InputAdornment>
+              )
+            }
           />
-        ) : (
-          <span className="arrow"></span>
-        )}
-      </Button>
+        </Box>
 
-      {isOpenDocUpload && (
-        <DynamicDocUploadModal
-          handleFileUploadSubmit={
-            handleFileUploadSubmit
-              ? () => handleFileUploadInExistingThread()
-              : () => {}
-          }
-          threadId={threadId}
-          userInputText={text}
-          open={isOpenDocUpload}
-          handleClose={handleCloseDocUploadModal}
-        />
-      )}
-    </Box>
-    <LimitOver
-      open={limitDialog}
-      onClose={() => setLimitDialog(false)}
-      title={
-        limitType === 'ai-summaries'
-          ? 'Your Summary Generation Limit is Over'
-          : limitType === 'storage'
-            ? 'Your Storage Limit is Over'
-            : limitType === 'messages-documents'
-              ? 'Your Document Upload Limit is Over'
-              : 'Your Copilot Chats Limit is Over'
-      }
-      subtitle={
-        limitType === 'ai-summaries'
-          ? 'Summary'
-          : limitType === 'storage'
-            ? 'Storage'
-            : limitType === 'messages-documents'
-              ? 'Message-Documents'
-              : 'Chats'
-      }
-      stats={
-        limitType === 'ai-summaries'
-          ? fetchedUser?.summary_used
-          : limitType === 'storage'
-            ? fetchedUser?.storage
-            : limitType === 'messages-documents'
-              ? '-/-'
-              : fetchedUser?.chat_used
-      }
-    />
+        <Button
+          type="button"
+          variant="contained"
+          className="btn-arrow"
+          color="primary"
+          fullWidth
+          onClick={handleMessageSending}
+          disabled={isSendDisabled}
+        >
+          {isLoadingProp ? (
+            <CircularProgress
+              size={18}
+              sx={{ color: 'var(--Txt-On-Gradient)' }}
+            />
+          ) : (
+            <span className="arrow"></span>
+          )}
+        </Button>
+
+        {isOpenDocUpload && (
+          <DynamicDocUploadModal
+            handleFileUploadSubmit={
+              handleFileUploadSubmit
+                ? () => handleFileUploadInExistingThread()
+                : () => {}
+            }
+            threadId={threadId}
+            userInputText={text}
+            open={isOpenDocUpload}
+            handleClose={handleCloseDocUploadModal}
+          />
+        )}
+      </Box>
+      <LimitOver
+        open={limitDialog}
+        onClose={() => setLimitDialog(false)}
+        title={
+          limitType === 'ai-summaries'
+            ? 'Your Summary Generation Limit is Over'
+            : limitType === 'storage'
+              ? 'Your Storage Limit is Over'
+              : limitType === 'messages-documents'
+                ? 'Your Document Upload Limit is Over'
+                : 'Your Copilot Chats Limit is Over'
+        }
+        subtitle={
+          limitType === 'ai-summaries'
+            ? 'Summary'
+            : limitType === 'storage'
+              ? 'Storage'
+              : limitType === 'messages-documents'
+                ? 'Message-Documents'
+                : 'Chats'
+        }
+        stats={
+          limitType === 'ai-summaries'
+            ? fetchedUser?.summary_used
+            : limitType === 'storage'
+              ? fetchedUser?.storage
+              : limitType === 'messages-documents'
+                ? '-/-'
+                : fetchedUser?.chat_used
+        }
+      />
+      <PlanExpired
+        open={expiredDialog}
+        onClose={() => setExpireDialog(false)}
+      />
     </>
   );
 }
